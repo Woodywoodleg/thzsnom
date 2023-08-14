6d2bc251e6da501c7dd575a9a1cb30de5348eaf1
Woodywoodleg <126465536+Woodywoodleg@users.noreply.github.com>
2023-08-14 15:36:10 +0200

    Add files via upload

---
+++ snom_microscope_reader.py
@@ -0,0 +1,40 @@
+from PIL import Image
+import os
+import matplotlib.pyplot as plt
+
+class ImageLoader:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.image_list = self.load_images()
+    
+    def load_images(self):
+        image_list = []
+        for filename in os.listdir(self.folder_path):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                image_path = os.path.join(self.folder_path, filename)
+                image = Image.open(image_path)
+                image_list.append(image)
+        return image_list
+
+    def plot_images(self, images):
+        num_images = len(images)
+        plt.figure(figsize=(10, 6))
+        
+        for idx, image in enumerate(images):
+            plt.subplot(1, num_images, idx + 1)
+            plt.imshow(image)
+            plt.title(f"Image {idx + 1}")
+            plt.axis('off')
+        
+        plt.tight_layout()
+        plt.show()
+
+# Example usage
+if __name__ == "__main__":
+    folder_path = r'C:\Users\Hebla\OneDrive\DTU Fotonik\Ph.d\Data\THz_NanoFrazor_v2\2023-08-10 3787'
+    loader = ImageLoader(folder_path)
+    
+    loaded_images = loader.load_images()
+    loader.plot_images(loaded_images)
+
+
